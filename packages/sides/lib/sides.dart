b56a330a74a7e6ee6b3a382bbb551d4dbbcc56ec
davitkovam <61286663+davitkovam@users.noreply.github.com>
2021-07-01 00:39:48 +0200

    Added saving to server

--- packages/sides/lib/sides.dart
+++ packages/sides/lib/sides.dart
@@ -3,10 +3,14 @@ import 'package:path/path.dart' as path;
 
 import 'package:tflite/tflite.dart';
 import 'dart:io';
+import 'dart:convert';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'package:path/path.dart';
+import 'package:http_parser/http_parser.dart';
 
 import 'package:flutter/material.dart';
 
@@ -63,17 +67,30 @@ uploadImage(File? image, String? pred, String real) async               //In pro
 {
   if(await internetAvailable() == 1)
     {
-      /*
+      var now = DateTime.now();
+      var formatter = DateFormat('yyyyMMdd_HH_mm_ss');
+      String currentTimeStamp = formatter.format(now);
+      print(currentTimeStamp);
+
+      //XY_Cars -> X = predicted, Y=real
+      var X = '${pred![0]}'.toUpperCase();
+      var Y = '${real[0]}'.toUpperCase();
+
+      String fname = X.toString()+Y.toString()+"_" + "Cars"+ currentTimeStamp+".jpg";
+
       print("Internet Available");
         var stream = new http.ByteStream(DelegatingStream.typed(image!.openRead()));
-        var length = await image!.length();
+        var length = await image.length();
 
         var uri = Uri.parse("https://carsides.coci.result.si/upload.php");
 
-        var request = new http.MultipartRequest("POST", uri);
-        var multipartFile = new http.MultipartFile('file', stream, length,
-            filename: basename(image.path)),
+        var request = new http.MultipartRequest("POST", uri)
+      ..fields['name'] = fname
+      ..fields['User-Agent']= "mememe";
+        var multipartFile = new http.MultipartFile('image', stream, length,
+            filename: fname,
         contentType: new MediaType('image', 'jpg'));
+
 
         request.files.add(multipartFile);
         var response = await request.send();
